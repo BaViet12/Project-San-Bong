0e509b7d7b3affaa6452da9cfc7c102d9b7e223e
BaViet12 <hpa220377@gmail.com>
2024-11-12 10:13:54 +0700

    second comit

--- app/api/soccer/[id]/route.ts
+++ app/api/soccer/[id]/route.ts
@@ -19,6 +19,7 @@ export async function DELETE(
     );
   }
 }
+
 export async function GET(
   requets: NextRequest,
   { params }: { params: { id: string } }
---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,16 @@
+import NavbarDashboard from "../components/NavbarDashboard";
+
+export default function Dashboard({
+    children,
+}:{
+    children:React.ReactNode
+}) {
+    return(
+        <div>
+            <NavbarDashboard/>
+            <div>
+                
+            </div>
+        </div>
+    );
+}
